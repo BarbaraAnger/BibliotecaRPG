72e9748a83d55cee87e60b063800aaa84d944cad
barbaraanger <barbaraanger@hotmail.com>
2019-11-27 15:18:18 -0300

    initialization to node express + angular

---
+++ angular-node-express/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { InputUserDataFormComponent } from './input-user-data-form/input-user-data-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputUserDataFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-node-express/src/app/input-user-data-form/input-user-data-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-user-data-form',
+  templateUrl: './input-user-data-form.component.html',
+  styleUrls: ['./input-user-data-form.component.css']
+})
+export class InputUserDataFormComponent implements OnInit {
+  registered = false;
+  submitted = false;
+  userForm: FormGroup;
+constructor(private formBuilder: FormBuilder)
+{
+
+}
+invalidFirstName()
+{
+  return(this.submitted && this.userForm.controls.first_name.erros != null);
+}
+invalidEmail()
+{
+return (this.submitted && this.userForm.controls.email.erros != null);
+}
+invalidZipCode()
+{
+  return (this.submitted && this.userForm.controls.zipcode.erros != null);
+}
+invalidPassword()
+{
+  return (this.submitted && this.userForm.controls.password.erros != null);
+}
+ngOnInit()
+{
+  this.userForm = this.formBuilder.group({
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    zipcode: ['', [Validators.required, Validators.pattern('^[0-9]{5}(?:-[0-9]{4})?$')]],
+  		password: ['', [Validators.required, Validators.minLength(5), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')]],
+  });
+}
+onSubmit() {
+  this.submitted = true;
+  if(this.userForm.invalid == true) {
+    return;
+  }
+  else
+  {
+    this.registered = true;
+  }
+}
+}
